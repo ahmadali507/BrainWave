0e50913559a46e50d223ca0769cbc54266fe2b62
ahmadali507 <ashahid.bese23seecs@seecs.edu.pk>
2024-04-28 12:28:57 +0500

    add

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Benefits from "./components/Benefits";
 import Collaboration from "./components/Collaboration";
+import Services from "./components/Services";
 function App() {
   return (
     <>
@@ -15,6 +16,7 @@ function App() {
       <Hero />
       <Benefits/>
       <Collaboration/>
+      <Services/>
     </div>
     <ButtonGradient/>
   </>
