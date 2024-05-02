4d943b684d8716e705961832f40612c1063dc8ba
ahmadali507 <ashahid.bese23seecs@seecs.edu.pk>
2024-05-03 00:44:35 +0500

    about

--- my-project/src/Pages/About.jsx
+++ my-project/src/Pages/About.jsx
@@ -1,36 +1,58 @@
-import React from 'react';
-import Section from '../components/Section';
-import Header from '../components/Header';
-import { Gradient } from '../components/design/Hero';
-import { BackgroundCircles } from '../components/design/Header';
-import { BottomLine } from '../components/design/Hero';
-import { curve } from '../assets';
-import NewsLetter from '../components/NewsLetter';
+import React from "react";
+import Section from "../components/Section";
+import Header from "../components/Header";
+import { Gradient } from "../components/design/Hero";
+import { BackgroundCircles } from "../components/design/Header";
+import { BottomLine } from "../components/design/Hero";
+import { curve } from "../assets";
+import NewsLetter from "../components/NewsLetter";
+import { motion } from "framer-motion";
+import  Typewriter  from "typewriter-effect";
+import MyFooter from "../components/MyFooter";
 const About = () => {
   return (
     <>
-      <Section Crosses className="mr-5 px-5 py-10">
+      <Section className="px-0 py-10">
         <Header />
 
-        <div className="relative top-15 mr-4 ml-4 flex-col md:mx-auto lg:mx-auto justify-center gap-14 w-[24.4rem] h-[29rem] md:w-[70rem] md:h-[70rem] lg:w-[70rem] lg:h-[80rem] backdrop-blur-sm md:text-wrap">
-
+        <div className="relative top-15 mr-4 ml-4 flex-col md:mx-auto lg:mx-auto justify-center gap-14 w-[24.4rem] h-[29rem] md:w-[70rem] md:h-[20rem] lg:w-[70rem] lg:h-[30rem] md:text-wrap">
           <div className="flex-col md:flex-col">
             <div className="text-center text-[1.5rem] md:text-[4rem] pb-10 md:pb-10 lg:h-16 lg:font-bold">
               BRAINWAVE
             </div>
-              <img src={curve} className='left-0 w-auto xl:-mt-2 mb-4 ' width={6} height={28} alt='curve'/>
+            <img
+              src={curve}
+              className="left-0 w-auto xl:-mt-2 mb-4 "
+              width={6}
+              height={28}
+              alt="curve"
+            />
             <div className="text-center text-[1.5rem] md:text-[3rem] md:text-center md:h-auto wx-auto">
               A SOLUTION BY BRAINSTORMERS.
             </div>
           </div>
-            <BottomLine />        
-          <div className="absolute top-75 mt-6 mx-auto text-white-200 py-20 text-neutral-500 backdrop-blur-md border text-center border-t-1 text-lg md:text-[2rem] md:h-[20rem] lg:h-[20rem] lg:text-center mx-30 px-6">
-            WE are a team of highly skilled and professional AI Engineers that are eager to provide you with the best of the AI solutions.
+          <BottomLine />
+          <div className="absolute top-175 mt-6 text-white-200 ml-12 py-10 text-neutral-500 backdrop-blur-md border text-center border-t-1 text-lg md:text-[2rem] md:h-[15rem] lg:h-[10rem] lg:text-center mx-30 sm:px-6">
+            <Typewriter
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString(
+                    "WE are a team of highly skilled and professional AI Engineers that are eager to provide you with the best of the AI solutions."
+                  ) // Replace this with your desired text
+                  .start();
+
+              }}
+              options = {{
+                delay:50,
+              }}
+            />
           </div>
         </div>
-        <NewsLetter/>
+        <NewsLetter />
         <BackgroundCircles />
       </Section>
+
+      <MyFooter/>
     </>
   );
 };
--- my-project/src/components/NewsLetter.jsx
+++ my-project/src/components/NewsLetter.jsx
@@ -3,6 +3,22 @@ import React from 'react'
 const NewsLetter = () => {
   return (
     <div>
+        <div className='flex flex-col md:flex-row lg:flex-row justify-center items-center  mx-6 bg-n-1/10 py-10
+        md:items-start lg:items-start md:justify-start md:px-20'>
+            <div className=' text-lg font-grotesk text-wrap py-2 px-3 '>
+                SIGN UP TO RECEIVE THE LATEST FROM US
+            </div>
+            <div className='md:pl-28 lg:pl-64 lg: px-3 py-2 font-grotesk'>
+
+                <div className='md:absolute  lg:absolute  flex-col justify-center items-center md:flex-row lg:justify-end   md:w-[38rem] lg:w-[38rem]'>
+
+                <input type="email" className='bg-n-4 rounded-md w-[15rem] h-[2rem] placeholder:text-stone-100 px-4' placeholder='Your Email goes here'/>
+                <button className='bg-green-600 rounded-md w-[9rem] h-[2rem]  '>SIGN UP</button>
+
+                </div>
+            </div>
+
+        </div>
       
     </div>
   )
